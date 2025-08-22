fa2c4f85cd0411b1db6e8cf1535b5f5afa7c467c
KawaaBongaaa <alvoka.studio@gmail.com>
2025-08-22 19:35:47 +0700

    Add files via upload

--- app_modern.js
+++ app_modern.js
@@ -2131,7 +2131,7 @@ window.toggleTheme = () => appState.toggleTheme();
 window.showHistory = showHistory;
 window.showGeneration = showGeneration;
 //window.selectStyle = selectStyle;
-window.selectStyle = (s) => window.setCarouselStyle(s).
+window.selectStyle = (s) => window.setCarouselStyle(s);
 window.generateImage = generateImage;
 window.newGeneration = newGeneration;
 window.cancelGeneration = cancelGeneration;
