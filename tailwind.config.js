abf49a7d85fe4ac403c87e54371090ef543adcfe
KawaaBongaaa <KawaaBongaaa@users.noreply.github.com>
2026-05-15 01:58:54 +0000

    deploy: a6cfca5c6953d7c74618308d1ff531621404f3c4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./*.html",
+    "./**/*.html",
     "./*.js",
     "./**/*.js",
     "./css/**/*.css",
