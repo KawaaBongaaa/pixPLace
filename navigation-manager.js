22cb120ccf1fe146527b3af479f8eb5ebb8dd61f
KawaaBongaaa <KawaaBongaaa@users.noreply.github.com>
2026-05-31 04:41:49 +0000

    deploy: 6f68648e4f06aefcb49823b3c5d71e2d0d5a8f9e

--- navigation-manager.js
+++ navigation-manager.js
@@ -895,7 +895,7 @@ export async function openSubscriptionPlans() {
             }
         }
 
-        const { showPricingModal } = await import('./js/modules/ui-utils.js?v=1780202468-1');
+        const { showPricingModal } = await import('./js/modules/ui-utils.js?v=1780202504-1');
         showPricingModal({ initialTab: 'plans' });
     } catch (e) {
         console.error('❌ Error in openSubscriptionPlans:', e);
@@ -914,7 +914,7 @@ export async function openCreditPacks() {
             }
         }
 
-        const { showPricingModal } = await import('./js/modules/ui-utils.js?v=1780202468-1');
+        const { showPricingModal } = await import('./js/modules/ui-utils.js?v=1780202504-1');
         showPricingModal({ initialTab: 'credits' });
     } catch (e) {
         console.error('❌ Error in openCreditPacks:', e);
